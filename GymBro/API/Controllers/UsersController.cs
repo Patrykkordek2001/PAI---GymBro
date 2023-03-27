7d3f5d025fe8a79b2ae6f3c49ad8b19340ccd1e4
patryk.kordek <patryk.kordek@idealogic.pl>
2023-03-28 01:35:06 +0200

    Update UsersController.cs

--- GymBro/API/Controllers/UsersController.cs
+++ GymBro/API/Controllers/UsersController.cs
@@ -3,8 +3,28 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace GymBro.API.Controllers
 {
+  [Route("api/[controller]")]
   public class UsersController : Controller
   {
+    [HttpPost("register")]
+    public async Task<IActionResult> Register()
+    {
+      return Ok();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetUserById()
+    {
+      return Ok();
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateUser()
+    {
+
+      return Ok();
+    }
+
 
   }
 }
